c4e0594ff8930fe2fb54b97d0a99137fdc4e9e7e
wday8 <moritz.woitag@gmail.com>
2022-01-29 01:07:00 +0100

    changed some lines

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/venus/venus-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derp
+    $(LOCAL_PATH)/overlay-pe
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
